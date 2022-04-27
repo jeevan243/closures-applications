965d846a2e4a1d6f0b53d00952fc4d389ddb38e1
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-27 16:50:40 +0530

    toggle function added

---
+++ Toggle/index.js
@@ -0,0 +1,21 @@
+function toggler(arr) {
+    var arr = arr;
+    let index = -1;
+    return function () {
+        index = index + 1;
+        if (index >= arr.length) {
+            index = 0;
+
+        }
+        return arr[index]
+
+    }
+}
+const toggle = toggler([1, 2, 3])
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
